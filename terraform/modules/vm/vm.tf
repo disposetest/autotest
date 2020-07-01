41b92400247dd708e1fac2534e11b8e39cef1c56
disposetest <67467118+disposetest@users.noreply.github.com>
2020-07-01 09:38:26 +0100

    Add files via upload

--- terraform/modules/vm/vm.tf
+++ terraform/modules/vm/vm.tf
@@ -2,14 +2,14 @@
 
 resource "azurerm_network_interface" "nic" {
   name                = "${var.application_type}-${var.resource_type}"
-  location            = "${var.location}"
-  resource_group_name = "${var.resource_group}"
+  location            = var.location
+  resource_group_name = var.resource_group
 
   ip_configuration {
     name                          = "internal"
-    subnet_id                     = "${var.subnet_id}"
+    subnet_id                     = var.subnet_id
     private_ip_address_allocation = "Dynamic"
-    public_ip_address_id          = "${var.public_ip_address_id}"
+    public_ip_address_id          = var.public_ip_address_id
   }
 }
 
@@ -18,7 +18,7 @@ resource "azurerm_network_interface" "nic" {
 resource "random_id" "randomId" {
     keepers = {
         # Generate a new ID when a new resource group is defined
-        resource_group = "${var.resource_group}"
+        resource_group = var.resource_group
     }
     
     byte_length = 8
@@ -27,35 +27,28 @@ resource "random_id" "randomId" {
 #An account to store the system diagnose
 resource "azurerm_storage_account" "SA" {
     name                        = "diagnose${random_id.randomId.hex}"
-    resource_group_name         = "${var.resource_group}"
-    location                    = "${var.location}"
+    resource_group_name         = var.resource_group
+    location                    = var.location
     account_replication_type    = "LRS"
     account_tier                = "Standard"
 
     tags = {
-        environment = "Terraform"
+        environment = "LinuxVMEnv"
     }
 }
 
-#Generating a key
-resource "tls_private_key" "pkey" {
-  algorithm = "RSA"
-  rsa_bits = 4096
-}
-
-output "tls_private_key" { value = "tls_private_key.pkey.private_key_pem" }
 
 resource "azurerm_linux_virtual_machine" "lvm" {
   name                = "${var.application_type}-${var.resource_type}-lvm"
-  location            = "${var.location}"
-  resource_group_name = "${var.resource_group}"
+  location            = var.location
+  resource_group_name = var.resource_group
   size                = "Standard_B1s"
   admin_username      = "testuser"
   disable_password_authentication = true
   network_interface_ids = [azurerm_network_interface.nic.id]
   admin_ssh_key {
     username   = "testuser"
-    public_key = tls_private_key.pkey.public_key_openssh
+    public_key = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAACAQD2DdxPjYlEW96uIweui4EyRDLh6zP52Mu5eLAuWyQlxgzph2Zw9Im/TpVfemw1NZGLQYL5rd+kMALPM7DglLjW/XINJ/GG+XP778SgnDXBtm9FCQP/GIgRuMYTPSEvtHt16WwL1TWIHbcBO17frd5aZ300GUCSviLw4q7i6EhHMcuNlAMTuTfNrwxW48TuH2C+11qItDJZdzHyBhu9ncFweRa7af2NqFLTVNuIQtTRz/7hnuTGXf96o61TYDJY/YSqCWT7oEYCAkjvXfLgJ9Egdy19m3RBs0OGLYk3loRn7WBfUoyN/Uuq8NIwvg+cbouRrnWXBLf0DykfZqMs3IOAnd0NFyftbVxiQtO4lVXTYuIJtKpDL+mHVnPvIjKykXQxPGTYAbsKqU96aLW0AUTWvtfnwstKtOkmuw3QNK/i0HSuAoup/gEgFmxsm24BUcyYRPQxemNmxlQbm9iUQUIX8fLdaBI+AGTeBUyuRepyhOR/l8ssFU2rexFAfTvKXiFzbu/dZ0dBBLFKPSH7nsSgHQsovx/gWxqfzVllFyS945cWKNWa0wjmLFO0zQx5mOly82KXKJXIxo6VSbTsc7sKbSbYwApETCNT0edi8kgSVjj4p5H1uiUn6EcBBVMX6ELadIh4y3AgZ6bYm/jNtdnV4pJ00PrxGJ21x5uokziC7w== cjordan@cjordan-Aspire-VX5-591G"
   }
   os_disk {
     caching           = "ReadWrite"
--- terraform/terraform.tfvars
+++ terraform/terraform.tfvars
@@ -1,8 +1,8 @@
 # Azure subscription vars
-subscription_id = ""
-client_id = ""
-client_secret = ""
-tenant_id = ""
+subscription_id = "520b1771-2700-4340-89c6-560a52f65ee9"
+client_id = "26b6e71c-5dea-4977-969f-a9837ef76432"
+client_secret = "yg6Z1WGQ08Uqvk8-_Qa1gMXxWTlv-LxZKX"
+tenant_id = "2cfcb216-ce47-4699-a281-2f1ee48e20f7"
 
 # Resource Group/Location
 location = "East US"
