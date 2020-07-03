2a03019c1d8eff9d3a03845dbf80bf5de92b65e2
disposetest <67467118+disposetest@users.noreply.github.com>
2020-07-03 16:13:08 +0100

    Add files via upload

--- automatedtesting/selenium/login.py
+++ automatedtesting/selenium/login.py
@@ -5,23 +5,21 @@ from selenium.common.exceptions import NoSuchElementException
 
 user = "standard_user"
 password = "secret_sauce"
-# Start the Browser
-
-print ('Starting the browser...')
 
 options = ChromeOptions()
-options.add_argument("--headless") 
+#options.add_argument("--headless") 
 options.add_argument("--no-sandbox") 
 options.add_argument("--disable-dev-shm-usage")
 #options.add_argument("disable-infobars")
 #options.add_argument("--disable-dev-shm-usage")
 driver = webdriver.Chrome(options=options)
 
-print ('Browser started successfully. Navigating to the demo page to login.')
-
 # Login
 def login (user, password):
     'Login with standard_user'
+
+    print ('Browser started successfully. Navigating to the demo page to login.')
+
     driver.get('https://www.saucedemo.com/')
 
     print('filling in the user name')
@@ -34,38 +32,58 @@ def login (user, password):
     try:
         driver.find_element_by_css_selector("input[value='LOGIN']")
     except NoSuchElementException: 
-        print('Login successfull')
+        print('\"'+ user + '\" was successfully logged in.')
         return True
-    print('Login failed')
+    print('Login failed for \"'+ user + '\".')
+    exit()
+    return False
 
 def addCart():
+    'Add store items to the cart'
     print('Adding items to cart')
-    items = driver.find_elements_by_xpath("//button[@class='btn_primary btn_inventory']")
+    #items = driver.find_elements_by_css_selector("button[class='btn_primary btn_inventory']")
+    items = driver.find_elements_by_css_selector("div[class='inventory_item']")
+
     for item in items:
-        item.click()
+
+        item.find_element_by_css_selector("button[class='btn_primary btn_inventory']").click()
+        #test if item was added or not
+        try:
+            item.find_element_by_css_selector("button[class='btn_primary btn_inventory']")
+        except NoSuchElementException:
+            print("Added  " + item.find_element_by_css_selector("div[class='inventory_item_name']").text)
+                       
+        
     count = driver.find_element_by_css_selector("span[class='fa-layers-counter shopping_cart_badge']").text
     if "6" in count:
-        print("All items where added")
+        print("All "+ count +" items where added")
     else:
-        print(count + " Items where added to cart")
+        print("Only " + count + " items where added to cart")
 
 
 def removeCart():
+    'Remove store items from the cart'
    
     print('Removing items from cart')
-    items = driver.find_elements_by_xpath("//button[@class='btn_secondary btn_inventory']")
+    items = driver.find_elements_by_css_selector("div[class='inventory_item']")
     for item in items:
-        item.click()
-    try:
-        driver.find_element_by_xpath("//span[@class='btn_primary btn_inventory']")
-    except NoSuchElementException:
-        print('All items where removed')
-        return False
-    print('All Items where not removed')
-    return True
 
+        item.find_element_by_css_selector("button[class='btn_secondary btn_inventory']").click()
+        #test if item was removed or not
+        try:
+            item.find_element_by_css_selector("button[class='btn_secondary btn_inventory']")
+        except NoSuchElementException:
+            print("removed  " + item.find_element_by_css_selector("div[class='inventory_item_name']").text)
+
+# Start
+print()
+print ('--------------Starting the browser-----------------')      
+   
 #execution
 login(user, password)
 addCart()
 removeCart()
+
+#end
+print("-------------------------End----------------------------------")
 
