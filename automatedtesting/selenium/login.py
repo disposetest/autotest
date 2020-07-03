2db629e47b37699dfcf76bbc1ccf4432e59dfcb7
disposetest <67467118+disposetest@users.noreply.github.com>
2020-07-03 21:38:34 +0100

    Add files via upload

--- automatedtesting/selenium/login.py
+++ automatedtesting/selenium/login.py
@@ -7,7 +7,7 @@ user = "standard_user"
 password = "secret_sauce"
 
 options = ChromeOptions()
-#options.add_argument("--headless") 
+options.add_argument("--headless") 
 options.add_argument("--no-sandbox") 
 options.add_argument("--disable-dev-shm-usage")
 #options.add_argument("disable-infobars")
